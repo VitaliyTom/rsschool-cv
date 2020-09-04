95029f7cd5198a7d1f6bd8503c763c076af1abc6
Vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-09-04 17:20:04 +0300

    refactor: content portfolio

--- content_porfolio.js
+++ content_porfolio.js
@@ -1,86 +1,94 @@
-export const result = {
+export const contentPortfolio = {
   js: {
     jsContent: `<div class="wrapper_calendar">
-  <div class="calendar"></div>
-  <p class="date">02.2020 - 07.2020</p>
+    <div class="calendar"></div>
+    <p class="date">02.2020 - 07.2020</p>
 </div>
-<li class="developing-li">Projects RS school:
-  <ul class="developing-ul">
+<li class="developing-li">
+    <span class="course">Projects RS school:</span>
 
-  
-  <li>
-          <a href="https://rslang-group51-blacktruth.netlify.app" target="_blank"
-              rel="noopener noreferrer">"RS Lang" on netlify</a> 
-              <span> - group development</span>
-      </li>
-      <li>
-          <a href="https://vitaliytom-rs2020q1-fancy-weather.netlify.app" target="_blank"
-              rel="noopener noreferrer">"fancy-weather" on netlify</a>
-      </li>
-      <li>
-          <a href="https://vitaliytom-rs2020q1-moviesearch.netlify.app" target="_blank"
-              rel="noopener noreferrer">"moviesearch" on netlify</a>
-      </li>
-      <li>
-          <a href="https://vitaliytom-rs2020q1-english-for-kids.netlify.app" target="_blank"
-              rel="noopener noreferrer">"english-for-kids" on netlify</a>
-      </li>
-      <li>
-          <a href="https://vitaliytom-rs2020q1-presentation.netlify.app" target="_blank"
-              rel="noopener noreferrer">"presentation" on netlify</a>
-      </li>
-      <li>
-          <a href="https://vitaliytom.github.io/codejam-virtual-keyboard" target="_blank"
-              rel="noopener noreferrer">"virtual-keyboard" on GitHub Pages</a>
-      </li>
+    <ul class="developing-ul">
+        <li>
+            <a href="https://rslang-group51-blacktruth.netlify.app" target="_blank" rel="noopener noreferrer">"RS
+                Lang" learning English</a>
+            <span> - group dev.</span>
+        </li>
+        <li>
+            <a href="https://vitaliytom-rs2020q1-fancy-weather.netlify.app" target="_blank"
+                rel="noopener noreferrer">"fancy-weather" - weather forecast</a>
+        </li>
+        <li>
+            <a href="https://vitaliytom-rs2020q1-moviesearch.netlify.app" target="_blank"
+                rel="noopener noreferrer">"moviesearch" - movie search</a>
+        </li>
+        <li>
+            <a href="https://vitaliytom-rs2020q1-english-for-kids.netlify.app" target="_blank"
+                rel="noopener noreferrer">"english-for-kids" - learning English</a>
+        </li>
+        <li>
+            <a href="https://vitaliytom-rs2020q1-presentation.netlify.app" target="_blank"
+                rel="noopener noreferrer">"presentation" - linux kernel</a>
+        </li>
+        <li>
+            <a href="https://vitaliytom.github.io/codejam-virtual-keyboard" target="_blank"
+                rel="noopener noreferrer">"virtual-keyboard" - virtual keyboard</a>
+        </li>
 
-      <li>
-          <a href="https://vitaliytom.github.io/singolo" target="_blank"
-              rel="noopener noreferrer">"singolo" on GitHub Pages</a>
-      </li>
+        <li>
+            <a href="https://vitaliytom.github.io/singolo" target="_blank" rel="noopener noreferrer">"singolo" - landing page</a>
+        </li>
 
-  </ul>
+    </ul>
 </li>`,
     frontEnd: `<p class="position">Front-end Developer</p>`,
   },
 
   java: {
     javaContent: `<div class="wrapper_calendar">
-  <div class="calendar"></div>
-  <p class="date">12.2018 - 06.2019</p>
+    <div class="calendar"></div>
+    <p class="date">12.2018 - 06.2019</p>
 </div>
-<li class="developing-li">Application "Animals" was made within the java course in <a
-      href="https://teachmeskills.by/">&lt;TeachMeSkills/&gt;</a>
-  <ul class="developing-ul">
-      <li>
-          <a href="https://github.com/VitaliyTom/animals" target="_blank"
-              rel="noopener noreferrer">"Animals" on GitHub</a>
-      </li>
-      <li>
-          <a href="http://93.125.1.133:88/animal" target="_blank"
-              rel="noopener noreferrer">"Animals" on the home linux-server</a>
-      </li>
-  </ul>
+<li class="developing-li">
+    <span class="course">
+        Application "Animals" was made within the java course in
+    
+    <a href="https://teachmeskills.by/">&lt;TeachMeSkills/&gt;</a>
+    </span>
+    <ul class="developing-ul">
+        <li>
+            <a href="https://github.com/VitaliyTom/animals" target="_blank" rel="noopener noreferrer">"Animals" on
+                GitHub</a>
+        </li>
+        <li>
+            <a href="http://93.125.1.133:88/animal" target="_blank" rel="noopener noreferrer">"Animals" on the home
+                linux-server</a>
+        </li>
+    </ul>
 </li>
 <div class="wrapper_calendar">
-  <div class="calendar"></div>
-  <p class="date">March - September 2018</p>
+    <div class="calendar"></div>
+    <p class="date">March - September 2018</p>
 </div>
-<li class="developing-li">Сourse projects
-  <ul class="developing-ul">
-      <li>
-          <a href="https://github.com/VitaliyTom/forumLogin" target="_blank"
-              rel="noopener noreferrer">"ForumLogin" on GitHub</a>
-      </li>
-      <li>
-          <a href="https://github.com/VitaliyTom/tic-tac-toe" target="_blank"
-              rel="noopener noreferrer">"Tic-tac-toe" on GitHub</a>
-      </li>
-      <li>
-          <a href="https://github.com/VitaliyTom/student/tree/master/src/com/company/students"
-              target="_blank" rel="noopener noreferrer">"Students" on GitHub</a>
-      </li>
-  </ul>
+<li class="developing-li">
+
+    <span class="course">
+    Сourse projects:
+    </span>
+
+    <ul class="developing-ul">
+        <li>
+            <a href="https://github.com/VitaliyTom/forumLogin" target="_blank"
+                rel="noopener noreferrer">"ForumLogin" on GitHub</a>
+        </li>
+        <li>
+            <a href="https://github.com/VitaliyTom/tic-tac-toe" target="_blank"
+                rel="noopener noreferrer">"Tic-tac-toe" on GitHub</a>
+        </li>
+        <li>
+            <a href="https://github.com/VitaliyTom/student/tree/master/src/com/company/students" target="_blank"
+                rel="noopener noreferrer">"Students" on GitHub</a>
+        </li>
+    </ul>
 </li>`,
     BackEnd: `<p class="position">Back-end Developer</p>`,
   },
