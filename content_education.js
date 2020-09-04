41d35337625887775952a8ec98bdbaaed34e3a2f
Vitaliytom <tomashevich.vitaliy88@gmail.com>
2020-09-04 17:19:26 +0300

    feat: add content education

---
+++ content_education.js
@@ -0,0 +1,75 @@
+export const contentEducation = {
+  js: {
+    jsContent: `<div class="wrapper_calendar">
+        <div class="calendar"></div>
+        <p class="date">02.02.2020 - 31.07.2020</p>
+    </div>
+    <p class="content_text">Successfully completed training at
+        <a href="https://rs.school/js" class="rs_school" target="blank" rel="noopener noreferrer">RS
+            school</a>
+        -
+        <a href="https://app.rs.school/certificate/agvivayo" class="rs_school_certificate"
+            target="blank" rel="noopener noreferrer">certificate</a>
+    </p>
+    <div class="wrapper_skills">
+        <div class="wrapper_Primary_Skills">
+            <span class="primary_Skills">Primary skills:</span>
+            <ul class="skills">
+                <li class="skills_Html">HTML</li>
+                <li class="skills_Css">CSS</li>
+                <li class="skills_Js">JavaScript</li>
+                <li class="skills_Git">Git/GitHub</li>
+                <li class="skills_Linux">linux</li>
+            </ul>
+        </div>
+        <div class="wrapper_Base_Skills">
+            <span class="base_Skills">Base skills:</span>
+            <ul class="skills">
+                <li class="skills_react">React</li>
+                <li class="skills_webpack">Webpack</li>
+                <li class="skills_eslint">Eslint</li>
+            </ul>
+        </div>
+    </div>
+
+    <div class="wrapper_calendar">
+        <div class="calendar"></div>
+        <p class="date">2005 - 2008</p>
+    </div>
+    <p class="content_text">3 years of study at the institute, University of civil protection</p>`,
+    frontEnd: `<p class="position">Front-end Developer</p>`,
+  },
+
+  java: {
+    javaContent: `<div class="wrapper_calendar">
+        <div class="calendar"></div>
+        <p class="date">03.2018 - 09.2018</p>
+    </div>
+    <p class="content_text">Java SE / Java EE in the programming school
+        <a href="https://teachmeskills.by/kursy-programmirovaniya/obuchenie-java-minsk"
+            class="teach_me_skills" target="blank" rel="noopener noreferrer">
+            &lt;TeachMeSkills/&gt;</a>
+        &nbsp; - <a href="https://app.rs.school/certificate/agvivayo" class="rs_school_certificate"
+            target="blank" rel="noopener noreferrer">certificate</a>
+    </p>
+    <div class="wrapper_skills">
+        <div class="wrapper_Primary_Skills">
+            <span class="primary_Skills">Primary skills:</span>
+            <ul class="skills java_Skill">
+                <li class="skills_Java">Java SE/EE</li>
+                <li class="skills_Spring">Spring</li>
+                <li class="skills_Hibernate">Hibernate</li>
+                <li class="skills_Tomcat">Tomcat</li>
+                <li class="skills_MySQL">MySQL</li>
+                <li class="skills_Maven">Maven</li>
+            </ul>
+        </div>
+    </div>
+    <div class="wrapper_calendar">
+        <div class="calendar"></div>
+        <p class="date">2005 - 2008</p>
+    </div>
+    <p class="content_text">3 years of study at the institute, University of civil protection</p>`,
+    BackEnd: `<p class="position">Back-end Developer</p>`,
+  },
+};
